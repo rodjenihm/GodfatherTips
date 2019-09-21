f6ed41390a528104ce4760da912d40729f0aabf8
Данијел Митровић <rodjenihm@outlook.com>
2019-09-22 00:47:53 +0200

    Edited AuthMessageSenderOptions.cs.

--- GodfatherTips/Utilities/AuthMessageSenderOptions.cs
+++ GodfatherTips/Utilities/AuthMessageSenderOptions.cs
@@ -7,5 +7,12 @@ namespace GodfatherTips.Utilities
 {
     public class AuthMessageSenderOptions
     {
+        public int PortNumber { get; set; }
+
+        public string SmtpServer { get; set; }
+
+        public string ApiUser { get; set; }
+
+        public string ApiKey { get; set; }
     }
 }
