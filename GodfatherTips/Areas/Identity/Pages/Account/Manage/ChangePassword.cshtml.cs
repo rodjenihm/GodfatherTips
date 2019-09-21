56aabe463e6cebbe7585c064ba936d1aede89b5f
Данијел Митровић <rodjenihm@outlook.com>
2019-09-22 01:31:03 +0200

    Scafolded and editedIdentity page Account/Manage/ChangePassword.

---
+++ GodfatherTips/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using GodfatherTips.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+namespace GodfatherTips.Areas.Identity.Pages.Account.Manage
+{
+    public class ChangePasswordModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger<ChangePasswordModel> _logger;
+
+        public ChangePasswordModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<ChangePasswordModel> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "{0} je obavezna")]
+            [DataType(DataType.Password)]
+            [Display(Name = "Trenutna šifra")]
+            public string OldPassword { get; set; }
+
+            [Required(ErrorMessage = "{0} je obavezna.")]
+            [StringLength(30, ErrorMessage = "{0} mora biti najmanje {2} a najviše {1} karaktera dugačka", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Nova šifra")]
+            public string NewPassword { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Potvrdite šifru")]
+            [Compare("NewPassword", ErrorMessage = "Šifre se ne podudaraju")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Neuspešno učitavanje korisnika ID: '{_userManager.GetUserId(User)}'");
+            }
+
+            var hasPassword = await _userManager.HasPasswordAsync(user);
+            if (!hasPassword)
+            {
+                return RedirectToPage("./SetPassword");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Neuspešno učitavanje korisnika ID: '{_userManager.GetUserId(User)}'");
+            }
+
+            var changePasswordResult = await _userManager.ChangePasswordAsync(user, Input.OldPassword, Input.NewPassword);
+            if (!changePasswordResult.Succeeded)
+            {
+                foreach (var error in changePasswordResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return Page();
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            _logger.LogInformation("User changed their password successfully.");
+            StatusMessage = "Vaša šifra je uspešno promenjena";
+
+            return RedirectToPage();
+        }
+    }
+}
