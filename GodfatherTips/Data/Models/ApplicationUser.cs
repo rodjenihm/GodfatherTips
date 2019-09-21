f400649b145e6f1c94783250ce615b4ea60519b6
Данијел Митровић <rodjenihm@outlook.com>
2019-09-22 00:33:25 +0200

    Edited ApplicationUser.cs model.

--- GodfatherTips/Data/Models/ApplicationUser.cs
+++ GodfatherTips/Data/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using GodfatherTips.Utilities;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,16 @@ namespace GodfatherTips.Data.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string Nickname { get; set; }
+
+        public Role Role { get; set; }
+
+        public bool IsVipMember { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public DateTime VipMembershipExpirationDate { get; set; }
+
+        public IEnumerable<Post> Posts { get; set; }
     }
 }
