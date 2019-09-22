aaded9cf0561bb97e48ad827d82605096754c8a6
Данијел Митровић <rodjenihm@outlook.com>
2019-09-22 14:45:20 +0200

    Edited ChatHub.cs.

--- GodfatherTips/Hubs/ChatHub.cs
+++ GodfatherTips/Hubs/ChatHub.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using GodfatherTips.Data.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GodfatherTips.Hubs
 {
-    public class ChatHub
+    public class ChatHub : Hub
     {
+        public async Task SendMessage(Post post) =>
+            await Clients.All.SendAsync("receiveMessage", post);
     }
 }
