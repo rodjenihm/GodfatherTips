dd9086d5873d121268e82977493aaa585542c825
Данијел Митровић <rodjenihm@outlook.com>
2019-09-23 00:26:01 +0200

    Edited ChatController.cs.

--- GodfatherTips/Controllers/ChatController.cs
+++ GodfatherTips/Controllers/ChatController.cs
@@ -2,17 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GodfatherTips.Data;
+using GodfatherTips.Data.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GodfatherTips.Controllers
 {
     [Authorize(Roles = "Admin, Vip")]
     public class ChatController : Controller
     {
-        public IActionResult Index()
+        public readonly ApplicationDbContext _context;
+        public readonly UserManager<ApplicationUser> _userManager;
+
+        public ChatController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            ViewBag.CurrentUserName = currentUser.UserName;
+            var posts = await _context.Posts.ToListAsync();
+            return View(posts);
+        }
+
+        public async Task<IActionResult> Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                post.UserName = User.Identity.Name;
+                var sender = await _userManager.GetUserAsync(User);
+                post.AuthorId = sender.Id;
+                await _context.Posts.AddAsync(post);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            return RedirectToAction("Error", "Home");
         }
     }
 }
