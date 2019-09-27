c4a4e09612d4c03966fabbd0e0c405a715a69e8b
Данијел Митровић <rodjenihm@outlook.com>
2019-09-28 00:24:14 +0200

    Scafolded TipsController.cs and Tips views.

---
+++ GodfatherTips/Controllers/TipsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GodfatherTips.Data;
+using GodfatherTips.Data.Models;
+
+namespace GodfatherTips.Controllers
+{
+    public class TipsController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public TipsController(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        // GET: Tips
+        public async Task<IActionResult> Index()
+        {
+            var tips = _dbContext.Tips.Include(t => t.Author);
+            return View(await tips.ToListAsync());
+        }
+
+        // GET: Tips/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tip = await _dbContext.Tips
+                .Include(t => t.Author)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tip == null)
+            {
+                return NotFound();
+            }
+
+            return View(tip);
+        }
+
+        // GET: Tips/Create
+        public IActionResult Create()
+        {
+            ViewData["AuthorId"] = new SelectList(_dbContext.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Tips/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nickname,Text,AuthorId,CreationDate")] Tip tip)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Add(tip);
+                await _dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_dbContext.Users, "Id", "Id", tip.AuthorId);
+            return View(tip);
+        }
+
+        // GET: Tips/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tip = await _dbContext.Tips.FindAsync(id);
+            if (tip == null)
+            {
+                return NotFound();
+            }
+            ViewData["AuthorId"] = new SelectList(_dbContext.Users, "Id", "Id", tip.AuthorId);
+            return View(tip);
+        }
+
+        // POST: Tips/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nickname,Text,AuthorId,CreationDate")] Tip tip)
+        {
+            if (id != tip.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _dbContext.Update(tip);
+                    await _dbContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TipExists(tip.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_dbContext.Users, "Id", "Id", tip.AuthorId);
+            return View(tip);
+        }
+
+        // GET: Tips/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tip = await _dbContext.Tips
+                .Include(t => t.Author)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tip == null)
+            {
+                return NotFound();
+            }
+
+            return View(tip);
+        }
+
+        // POST: Tips/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tip = await _dbContext.Tips.FindAsync(id);
+            _dbContext.Tips.Remove(tip);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TipExists(int id)
+        {
+            return _dbContext.Tips.Any(e => e.Id == id);
+        }
+    }
+}
