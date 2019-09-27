d15f3cb44e5b684e1106e0df22cfda987352d3f8
Данијел Митровић <rodjenihm@outlook.com>
2019-09-27 23:26:37 +0200

    Edited Chat/Index.cshtml view, edited site.css, edited chat.js: new chat appearance.

--- GodfatherTips/wwwroot/js/chat.js
+++ GodfatherTips/wwwroot/js/chat.js
@@ -1,7 +1,5 @@
-﻿class Message
-{
-    constructor(username, text, creationDate)
-    {
+﻿class Message {
+    constructor(username, text, creationDate) {
         this.userName = username;
         this.text = text;
         this.creationDate = creationDate;
@@ -15,8 +13,7 @@ const creationDateInput = document.getElementById('creationDate');
 const chat = document.getElementById('chat');
 const messagesQueue = [];
 
-document.getElementById('submitButton').addEventListener('click', () =>
-{
+document.getElementById('submitButton').addEventListener('click', () => {
     var currentdate = new Date();
     creationDate.innerHTML =
         (currentdate.getMonth() + 1) + "/"
@@ -25,50 +22,41 @@ document.getElementById('submitButton').addEventListener('click', () =>
         + currentdate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 });
 
-function clearInputField()
-{
+function clearInputField() {
     messagesQueue.push(textInput.value);
     textInput.value = "";
 }
 
-function sendMessage()
-{
+function sendMessage() {
     let text = messagesQueue.shift() || "";
     if (text.trim() === "") return;
-    
+
     let creationDate = new Date();
     let message = new Message(username, text);
     sendMessageToHub(message);
 }
 
-function addMessageToChat(message)
-{
+function addMessageToChat(message) {
     let isCurrentUserMessage = message.userName === username;
 
-    let containerClass = isCurrentUserMessage ? "container darker bg-primary" : "container bg-light";
-    let textAlign = isCurrentUserMessage ? "sender text-right text-white" : "sender text-left";
-    let timePosition = isCurrentUserMessage ? "time-right text-light" : "time-left";
-    let offset = isCurrentUserMessage ? "col-md-6 offset-md-6" : "";
+    let container = isCurrentUserMessage ? "msg_container_send" : "msg_container";
+    let justify = isCurrentUserMessage ? "d-flex justify-content-end mb-4" : "d-flex justify-content-begin mb-4";
+    let time = isCurrentUserMessage ? "msg_time_send" : "msg_time";
 
-    let divrow = document.createElement('div');
-    divrow.className = "row";
+    let div1 = document.createElement('div');
+    div1.className = justify;
 
-    let divoffset = document.createElement('div');
-    divoffset.className = offset;
+    let div2 = document.createElement('div');
+    div2.className = container;
 
-    let container = document.createElement('div');
-    container.className = containerClass;
-
+    let senderi = document.createElement('i');
+    senderi.style = "color: darkred";
+    senderi.innerHTML = message.userName;
     let sender = document.createElement('p');
-    sender.className = textAlign;
-    sender.innerHTML = message.userName;
-
-    let text = document.createElement('p');
-    text.innerHTML = message.text;
-    text.classList = textAlign;
+    sender.innerHTML = message.text;
 
     let creationDate = document.createElement('span');
-    creationDate.className = timePosition;
+    creationDate.className = time;
 
     var currentdate = new Date();
     creationDate.innerHTML =
@@ -77,10 +65,9 @@ function addMessageToChat(message)
         + currentdate.getFullYear() + " "
         + currentdate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 
-    container.appendChild(sender);
-    container.appendChild(text);
-    container.appendChild(creationDate);
-    divoffset.appendChild(container);
-    divrow.appendChild(divoffset);
-    chat.appendChild(divrow);
+    div2.appendChild(senderi);
+    div2.appendChild(sender);
+    div2.appendChild(creationDate);
+    div1.appendChild(div2);
+    chat.appendChild(div1);
 }
