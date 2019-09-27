0ce6dc78ae8fac507128517985ad85b81d37ff84
Данијел Митровић <rodjenihm@outlook.com>
2019-09-28 00:01:43 +0200

    Added Tip.cs data model.

---
+++ GodfatherTips/Data/Models/Tip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GodfatherTips.Data.Models
+{
+    public class Tip
+    {
+        public int Id { get; set; }
+
+        public string Nickname { get; set; }
+
+        public string Text { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public ApplicationUser Author { get; set; }
+
+        public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+    }
+}
